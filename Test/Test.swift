3f40484733f6c83c00732b2114546a83d4f08e67
nagendra <nagendra.patel@yaantra.com>
2021-01-29 15:53:59 +0530

    intial

---
+++ Test/Test.swift
@@ -0,0 +1,21 @@
+//
+//  Test.swift
+//  Test
+//
+//  Created by YAANTRA on 29/01/21.
+//
+
+import Foundation
+public final class Test {
+
+    let name = "Test"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
